cd0d1e4d9f92c75fc8444de9ccce8395cd3e0320
cedcoss <abhishekshukla25jancedcoss.com>
2022-02-24 11:40:54 +0530

    file

--- src/file_upload.php
+++ src/file_upload.php
@@ -1,18 +1,18 @@
-<?php
-if(isset($_FILES["fileToUpload"])){
-    $uploadOk =1;
-    print_r($_FILES);
-    if ($_FILES["fileToUpload"]["size"] > 200000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-      }
+<!-- <?php
+// if(isset($_FILES["fileToUpload"])){
+//     $uploadOk =1;
+//     print_r($_FILES);
+//     if ($_FILES["fileToUpload"]["size"] > 200000) {
+//         echo "Sorry, your file is too large.";
+//         $uploadOk = 0;
+//       }
   
-    if ($uploadOk==1 && move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],"uploads/".$_FILES["fileToUpload"]["name"])) {
-        echo "The file has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
+//     if ($uploadOk==1 && move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],"uploads/".$_FILES["fileToUpload"]["name"])) {
+//         echo "The file has been uploaded.";
+//     } else {
+//         echo "Sorry, there was an error uploading your file.";
+//     }
+// }
 
 ?>
 <!DOCTYPE html>
@@ -30,4 +30,74 @@ if(isset($_FILES["fileToUpload"])){
         <input type="submit" value="Upload Image" name="submit">
     </form>
 </body>
+</html>
+ -->
+
+
+
+
+
+
+
+
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+echo $_FILES["fileToUpload"]["size"];
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 2000000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "png" ) {
+  echo "Sorry, only PNG files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>PHP-Forms</title>
+</head>
+
+<body>
+  <div class="wrapper">
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+  </div>
+</body>
+
 </html>
