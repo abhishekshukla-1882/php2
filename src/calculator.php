03fd2dfd575a446c06dd8744948aa9babc9c2773
abhishekshuklaofficial <98631720+abhishekshuklaofficial@users.noreply.github.com>
2022-02-24 11:26:58 +0530

    Update calculator.php

--- src/calculator.php
+++ src/calculator.php
@@ -1,52 +1,57 @@
 <?php
-$first_num = $_POST['val'];
-$second_num = $_POST['val1'];
-$operator = $_POST['operator'];
-$result = '';
-if (is_numeric($first_num) && is_numeric($second_num)) {
-    switch ($operator) {
-        case "Add":
-           $result = $first_num + $second_num;
-            break;
-        case "Subtract":
-           $result = $first_num - $second_num;
-            break;
-        case "Multiply":
-            $result = $first_num * $second_num;
-            break;
-        case "Divide":
-            $result = $first_num / $second_num;
-    }
-}
 
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<div id="page-cal">
-		<h1>Php - Calculator </h1>
-
-		<form action="" method="post" id="cal">
-            Firrst val : 	<input type="number" name="val" id="val" placeholder="Please enter no." /><br><br>
-            second val : <input type="number" name="val1" id="val1" placeholder="Please enter no." /><br><br>
-            <input type="submit" name="operator" value="Add" />&nbsp;&nbsp;&nbsp;
-            <input type="submit" name="operator" value="Subtract" />&nbsp;&nbsp;&nbsp;
-            <input type="submit" name="operator" value="Multiply" />&nbsp;&nbsp;&nbsp;
-            <input type="submit" name="operator" value="Divide" />
-		</form>
-
-		<div>
-		    <?php
-        // echo $sub;
-        echo '<br />' . $result; ?>
-		</div>
-	</div>
-</body>
+if(isset($_GET)){
+    $unit = $_GET['unit'];
+  }
+  if($unit <= 50){
+     $total = $unit * 3.50;
+  }
+  elseif($unit>50 and $unit <=150){
+     $total = (($unit-50)*4.00) + (50*3.50);
+  }
+  elseif($unit>150 and $unit <=250){ 
+   $unit = $unit - 50;
+   $total = 50*3.50;
+   $unit= $unit -100 ;
+   $total += (100 *4.00) + ($unit * 5.20);   
+ }
+ else{
+   $unit = $unit - 50;
+   $total = 50*3.50;
+   $unit= $unit -100 ;
+   $total += (100 *4.00);
+   $unit = $unit -100;
+   $total += (100 *5.20) + ($unit * 6.50); 
+ 
+ }
+ 
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ 
+ <head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="style.css">
+   <title>PHP-Forms</title>
+ </head>
+ 
+ <body>
+   <div class="wrapper">
+     <h3> Program to calculate Electricity bill </h3>
+     <form method="GET" action="" >
+       <div class="user">
+         <dv class="col">
+           <label for="Name" class="row">Unit</label>
+         </dv>
+         <div class="col">
+           <input type="text" id="unit" name ="unit" required>
+         </div>
+       </div>
+       <input type="submit" value="Calculate" id="submit" name="submit">
+     </form>
+     <H2>Total bill : Rs <?php echo $total?></h2>
+   </div>
+ </body>
 </html>
